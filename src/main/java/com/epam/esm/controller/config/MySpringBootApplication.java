3bae1c25b8de039d6a5212264604cf8e2040fd61
Yana Dziadkouskaya <yana_dziadkouskaya@gmail.com>
2021-05-03 17:27:11 +0300

    Fix bugs

--- src/main/java/com/epam/esm/controller/config/MySpringBootApplication.java
+++ src/main/java/com/epam/esm/controller/config/MySpringBootApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.epam.esm"})
 public class MySpringBootApplication {
-//    public static void main(String[] args) {
+   public static void main(String[] args) {
         SpringApplication.run(MySpringBootApplication.class, args);
 
     }
